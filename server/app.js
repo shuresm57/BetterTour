f110a427abd871d928f3e6bb030bd18f9441930f
shuresm57 <vsorensen31@gmail.com>
2026-05-15 09:46:20 +0200

    changed to server side rendering for deployment

--- server/app.js
+++ server/app.js
@@ -3,15 +3,12 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import helmet from 'helmet';
+import path from 'path';
 import authRouter from './routers/authRouter.js';
 import userRouter from './routers/userRouter.js';
 import contactRouter from './routers/contactRouter.js';
 
 const app = express();
-
-// ====================
-//  MIDDLEWARE
-// ====================
 
 app.use(helmet());
 app.use(cors({
@@ -21,13 +18,15 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-// ====================
-//  ROUTERS
-// ====================
+app.use(express.static('../client/dist'));
 
 app.use(authRouter);
 app.use(userRouter);
 app.use(contactRouter);
+
+app.get('/{*splat}', (req, res) => {
+  res.sendFile(path.resolve('../client/dist/index.html'));
+});
 
 const PORT = process.env.PORT ?? 3000;
 
